06c5906795c2d7209b574018525d66dbb5754457
ratinacage26 <vincent.power@gmail.com>
2017-08-23 22:03:51 -0400

    Work-next project complete

--- wp-content/themes/brandlucent Theme/template-parts/next-project-block.php
+++ wp-content/themes/brandlucent Theme/template-parts/next-project-block.php
@@ -1,5 +1,4 @@
 
- 
  <section class="next-project-block">
 
     <?php while( have_rows('next_project_block') ): the_row(); ?>
@@ -7,25 +6,30 @@
     	<div class="project-block-wrapper">
 
 	 		<h2 class="next-project">|Next Project|</h2>
-			<h1 class="service-title"> <?php echo get_sub_field('project_title') ;?> 
-	 		</h1>
 
+			<a href="<?php echo get_sub_field('learn_more_link') ;?>">
+				<h1 class="service-title"> <?php echo get_sub_field('project_title') ;?> 
+	 			</h1>
+	 		</a>
 
-			<?php while( have_rows('project_tags_list') ): the_row(); ?>
-				<a href="<?php echo get_sub_field('project_tag_link') ;?>">
-					<p><?php echo get_sub_field('project_tag') ;?></p>
-				</a>
-    		<?php endwhile; ?>
+			<div class="project-tag">
+				<?php while( have_rows('project_tags_list') ): the_row(); ?>
+					<a href="<?php echo get_sub_field('project_tag_link') ;?>">
+						<p><?php echo get_sub_field('project_tag') ;?></p>
+					</a>
+    			<?php endwhile; ?>
+			</div>
 
-
-			<?php 
-			if(get_sub_field('learn_more_text')) {
-				echo '<a class="learn-more-text href="'  . get_sub_field('learn_more_link') .  '"><div>See Project<i class="fa fa-long-arrow-right" aria-hidden="true"></i></div></a>';
-				} ?> 
-
-		</div>
-        
+	    	<a class="learn-more-text" href="<?php echo get_sub_field('learn_more_link') ;?>">
+					<div>See Project</div><i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+			</a>
+			
+		</div> 
     <?php endwhile; ?>
+
+	<?php if( get_field('next_project_image') ): ?>
+ 		<img class="next-project-image" src="<?php echo get_field('next_project_image')?>">
+	<?php endif; ?>
  
  </section>
  
